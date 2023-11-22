8e4f8b18af0fb9e5bdf4940b126ded39bcc99d13
Rabbear <1015445626@qq.com>
2023-11-22 19:23:42 +0800

    add 2D urban sprites；add basic build logic

---
+++ Assets/Scripts/BuildingSystem/TilemapLayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace BuildingSystem
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class TilemapLayer : MonoBehaviour
+    {
+        protected Tilemap m_tilemap { get; private set; }
+
+        private void Awake()
+        {
+            m_tilemap = GetComponent<Tilemap>();
+        }
+    }
+}
