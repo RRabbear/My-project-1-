91f5f6c11ba47648d5d8f60ad38201271559c6ec
WXC-HUB <54668505+WXC-HUB@users.noreply.github.com>
2023-11-24 14:07:44 +0800

    工具栏部分逻辑调整

--- Assets/Scripts/Core/LevelEventDefs.cs
+++ Assets/Scripts/Core/LevelEventDefs.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Assets.Scripts.GamePlay2D;
 
 namespace Assets.Scripts.Core
 {
@@ -22,9 +23,15 @@ namespace Assets.Scripts.Core
 
     }
 
-    public enum GamePlay2D_CMD_Character
+    public enum GamePlay2D_CMDType_Character
     {
-        Spawn_Hero
+        Character_Spawn,
+        Character_AddTool,
+    }
+
+    public enum CharacterSpwnType
+    {
+        AsHero
     }
 
     public class SpawnItemEventArgs : BaseEventArgs
@@ -33,6 +40,32 @@ namespace Assets.Scripts.Core
         public SpawnItemEventArgs(ObjectOperateEvent _t , GameObject _sender ,  string objectName) : base(_t , _sender)
         {
             ObjectName = objectName;
+        }
+    }
+
+    public class Character_CMD_SpawnEventArgs : BaseEventArgs
+    {
+        public Vector3 pos;
+        public CharacterSpwnType spwnType;
+        public GameObject SpawnObject;
+
+        public Character_CMD_SpawnEventArgs(GamePlay2D_CMDType_Character _t, GameObject _sender , Vector3 pos, CharacterSpwnType spwnType, GameObject spawnObject) : base(_t , _sender)
+        {
+            this.pos = pos;
+            this.spwnType = spwnType;
+            SpawnObject = spawnObject;
+        }
+    }
+
+    public class Character_CMD_AddTool : BaseEventArgs
+    {
+        public int toolID;
+        public CharacterBase toCharacter;
+
+        public Character_CMD_AddTool(GamePlay2D_CMDType_Character _t, GameObject _sender ,  int toolID, CharacterBase toCharacter) : base(_t , _sender)
+        {
+            this.toolID = toolID;
+            this.toCharacter = toCharacter;
         }
     }
 }
